7ad72ea7f9d7b156da506fad41af822d4439a362
Gwen Dipert <gwendipert@ziftsolutions.com>
2017-02-08 09:06:56 -0500

    deleted non-specific Emerson things

--- assets_check_sel.py
+++ assets_check_sel.py
@@ -69,59 +69,6 @@ class LinkTests(object):
 	def PDF(self):
 		self.driver.get(self.url)
 		WebDriverWait(self.driver,10)
-		svparents = self.driver.find_elements_by_tag_name('li')
-		for svparent in svparents:
-			if "clsSVAssetType_application_pdf" in svparent.get_attribute('class'):
-				svlink = svparent.find_element_by_tag_name('a')
-				svlinktitle = svlink.text.encode('ascii','ignore')
-				#title truncation handles Fuji Xerox
-				self.PDFTextCheck(svlinktitle[len(svlinktitle)-5:len(svlinktitle)-1],svlink.get_attribute('href'))
-			else:
-				pass
-		"""
-		links = self.driver.find_elements_by_tag_name('a')
-		Dell EMC
-		navs = self.driver.find_elements_by_xpath("//*[ancestor::li[@class='MenuMainItem']]")
-		subnavs = self.driver.find_elements_by_xpath("//*[ancestor::div[@name='Product Families']]")
-		Dell APJ:
-			navs = self.driver.find_elements_by_xpath(".//*[ancestor::div[@class='z_row header']]")
-			subnavs = self.driver.find_elements_by_xpath(".//*[ancestor::ul[@class='dropdown-menu solutionsdd sm-nowrap']]")
-		Nexsan:
-			nexsans = self.driver.find_elements_by_xpath(".//*[ancestor::div[@id='idSVNav']]")
-		VMWare:
-			menus = self.driver.find_elements_by_xpath(".//*[ancestor::span[@name='Showcase Nav']]")
-		Epicor:
-			tops = self.driver.find_elements_by_class_name('z_nav')
-		for link in links:
-			if link not in navs and link not in subnavs:
-				zlinks.append(link)
-		gated = []
-		for zlink in zlinks:
-			try:
-				if self.GateTest(zlink):
-					self.GetFormURL(zlink)
-					gated.append(zlink)
-				elif 'pdf' in zlink.get_attribute('href').split('.'):
-					self.PDFTextCheck(zlinktitle,zlink.get_attribute('href'))
-			except:
-				pass
-		windows = self.driver.window_handles
-		numwindows = len(self.driver.window_handles)
-		i = 1
-		if i == numwindows:
-			return
-			print "No gated PDFs"
-		else:
-			self.driver.switch_to_window(windows[1])
-			while i < numwindows:
-				self.PDFFormSubmit()
-				i += 1
-				if i == numwindows:
-					pass
-				else:
-					self.driver.switch_to_window(windows[i])
-		"""
-
 		zlinks = self.driver.find_elements_by_xpath("//span[@name='RES.Resource Name']/ancestor::*[position()=1]")
 		zlinknames = self.driver.find_elements_by_xpath("//span[@name='RES.Resource Name']")
 		for zlink in zlinks:
